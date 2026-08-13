7f53f63ee582af76d6c5b3c85c1065dc79d3da34
Michael Obed <mikee246@live.co.uk>
2026-08-13 20:35:10 +0100

    #3: Disabled sleep (but kept functions in code in case of future dev).

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -129,21 +129,23 @@ void Restart()
     constexpr uint32_t delayUs = 3000000UL;
 
     esp_rom_delay_us(delayUs);
+    ESP_LOGW(__func__, "*** RESET!!! ***");
     vTaskDelete(switchIOTaskHandle);
     lighting.DeInit();
     if(network.IsRunning())
         network.DeInit();
 
-    ESP_LOGW(__func__, "*** RESET!!! ***");
     esp_restart();
 }
 
 void Sleep()
 {
+    /* Currently unused - see issue #3. */
+
+    ESP_LOGW(__func__, "Sleeping now... -.-");
     if(network.IsRunning())
         network.DeInit();
     lighting.WaitForLEDsOff();
-    ESP_LOGW(__func__, "Sleeping now... -.-");
     esp_light_sleep_start();
     ESP_LOGW(__func__, "I'm awake! ^_^");
 }
@@ -161,8 +163,9 @@ void switchIOTask(void* arg)
             lightingStateLast = lightingState;
         }
 
-        if((config.GetConfigData("sleepMode") == Config::SleepMode_WhenLEDOff) && !lightingState)
-            Sleep();
+        /* We don't do this currently - see issue #3. */
+        // if((config.GetConfigData("sleepMode") == Config::SleepMode_WhenLEDOff) && !lightingState)
+        //     Sleep();
 
         vTaskDelay(100 / portTICK_PERIOD_MS);
     }
